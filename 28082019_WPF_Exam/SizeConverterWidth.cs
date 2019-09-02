2698b4f8fd5eb375eec4e35125150c98a010930a
ruslan261081 <45044693+ruslan261081@users.noreply.github.com>
2019-09-02 23:44:54 +0300

    Add files via upload

---
+++ 28082019_WPF_Exam/SizeConverterWidth.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace _28082019_WPF_Exam
+{
+    public class SizeConverterWidth : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double sliderValue = (double)value;
+            if (sliderValue < 51)
+                return "Small";
+            if (sliderValue > 50 && sliderValue < 71)
+                return "Medium";
+            if (sliderValue > 70 && sliderValue < 90)
+                return "Large";
+            return "Extra Large";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 28082019_WPF_Exam/ViewModel.cs
@@ -0,0 +1,83 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28082019_WPF_Exam
+{
+   public class ViewModel : INotifyPropertyChanged
+   {
+        private double _width;
+        private double _height;
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                this._width = value;
+                OnPropertyChanged("Width");
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                this._height = value;
+                OnPropertyChanged("Height");
+            }
+        }
+        private string text;
+        public string MyText
+        {
+            get
+            {
+                return this.text;
+            }
+            set
+            {
+                text = value;
+                OnPropertyChanged("MyTxtBx");
+            }
+        }
+
+        public DelegateCommand MyCommand { get; set; }
+
+        public ViewModel()
+        {
+            Width = 30;
+            Height = 30;
+            MyCommand = new DelegateCommand(ExecuteCommand, CanExecuteMethod);
+        }
+
+        private void OnPropertyChanged(string v)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void ExecuteCommand()
+        {
+
+        }
+        private bool CanExecuteMethod()
+        {
+            return true;
+
+        }
+   }
+}
